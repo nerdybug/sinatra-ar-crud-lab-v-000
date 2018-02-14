373b8ff2d9774edfd1489bf1e5637d2220f5aa02
Meredith Reece <mardebugger@gmail.com>
2018-02-14 23:31:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,10 @@ class ApplicationController < Sinatra::Base
   get '/posts/new' do
     erb :new
   end
+
+  post '/posts' do
+    Post.create(name: params[:name], content: params[:content])
+    @posts = Post.all
+    erb :index
+  end
 end
